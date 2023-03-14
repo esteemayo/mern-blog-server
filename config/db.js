369e3b7d8245e8259b75b7f3a28c00e69f4e55d5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 00:46:15 +0100

    feat: create a mongodb file

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
+
+const dbLocal = DATABASE_LOCAL;
+const mongoURI = DATABASE.replace('<PASSWORD>', DATABASE_PASSWORD);
+
+const db = devEnv ? dbLocal : mongoURI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db);
+    console.log(`Connected to MongoDB → ${conn.connection.port}`.gray.bold);
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = connectDB;
