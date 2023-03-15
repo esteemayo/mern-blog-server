6c7c741088eb093107b8a1b47a5d9ddf3a5354d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 01:17:23 +0100

    feat: convert to es6 module

--- routes/categories.js
+++ routes/categories.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const categoryController = require('../controllers/categoryController');
 const authController = require('../controllers/authController');
