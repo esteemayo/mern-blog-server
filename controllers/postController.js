d02c312d931bdda0cb9a7698af3c965b015cbaf4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 01:01:29 +0100

    feat: convert to es6 module

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 
-const Post = require('../models/Post');
-const catchAsync = require('../utils/catchAsync');
-const APIFeatures = require('../utils/apiFeatures');
-const NotFoundError = require('../errors/notFound');
-const ForbiddenError = require('../errors/forbidden');
+import Post from '../models/Post.js';
+import catchAsync from '../utils/catchAsync.js';
+import APIFeatures from '../utils/apiFeatures.js';
+import NotFoundError from '../errors/notFound.js';
+import ForbiddenError from '../errors/forbidden.js';
 
 exports.getAllPosts = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(Post.find(), req.query)
