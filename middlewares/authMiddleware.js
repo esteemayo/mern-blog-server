d5d6766d54474cc717babbbce32a9b172fdffa73
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 02:19:33 +0100

    feat: add restrictTo middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -41,3 +41,15 @@ export const protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ForbiddenError('You do not have permission to perform this action')
+      );
+    }
+
+    next();
+  };
+};
