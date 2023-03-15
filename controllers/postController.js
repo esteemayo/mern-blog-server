ba662a04abe61b9f676fce570d089641d3eb59d7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 00:55:09 +0100

    fix: format code

--- controllers/postController.js
+++ controllers/postController.js
@@ -80,10 +80,13 @@ export const updatePost = catchAsync(async (req, res, next) => {
   }
 
   if (post.username === req.user.username) {
-    const updatedPost = await Post.findByIdAndUpdate(postId, { $set: { ...req.body } }, {
-      new: true,
-      runValidators: true,
-    });
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $set: { ...req.body } },
+      {
+        new: true,
+        runValidators: true,
+      });
 
     return res.status(StatusCodes.OK).json({
       status: 'success',
