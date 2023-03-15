702cf925566e3a4dfd1f9cffe4acd0ff7a534c4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 00:58:00 +0100

    fix: format code

--- controllers/postController.js
+++ controllers/postController.js
@@ -103,7 +103,9 @@ export const deletePost = catchAsync(async (req, res, next) => {
   const post = await Post.findById(postId);
 
   if (!post) {
-    return next(new NotFoundError(`No post found with that ID → ${postId}`));
+    return next(
+      new NotFoundError(`No post found with that ID → ${postId}`)
+    );
   }
 
   if (post.username === req.user.username) {
