120f2a9b98afdf9860bebf56a093146d7c7aea35
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 02:55:40 +0100

    fix: modify

--- models/User.js
+++ models/User.js
@@ -145,6 +145,6 @@ userSchema.methods.createPasswordResetToken = function () {
   return resetToken;
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
