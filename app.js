074cd6da2b277f4d853c44a4baa673978147aec6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 02:13:30 +0100

    fix: change file name

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: './config.env' });
 
 // routes
 import categoryRoute from './routes/categories.js';
-import errorHandlerMiddleware from './controllers/errorController.js';
+import errorHandlerMiddleware from './middlewares/errorHandler.js';
 import userRoute from './routes/users.js';
 import BadRequestError from './errors/badRequest.js';
 import postRoute from './routes/posts.js';
