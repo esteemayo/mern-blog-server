ad0548e7dbc30c5ee91e47feb72cad6424a0b5f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-15 02:16:57 +0100

    fix: modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,6 @@
 import _ from 'lodash';
-import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import { StatusCodes } from 'http-status-codes';
-import { promisify } from 'util';
 
 import User from '../models/User.js';
 import catchAsync from '../utils/catchAsync.js';
