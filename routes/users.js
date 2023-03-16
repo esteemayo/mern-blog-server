900156d2424cec0882321c0864255005a07385f8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-16 02:33:55 +0100

    fix: modify

--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ router
 router
   .route('/:id')
   .get(userController.getUser)
-  .patch(userController.updateMe)
+  .patch(router.use(authMiddleware.restrictTo('admin')), userController.updateMe)
   .delete(userController.deleteUser);
 
 export default router;
